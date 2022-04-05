aa90080399e1db2dd33001aed7224611ba04bc3b
RiwooPapa <grooveray@gmail.com>
2022-04-05 17:53:58 +0900

    MODIFY/ controller.tweets.js def_validator

--- router/tweets.js
+++ router/tweets.js
@@ -6,26 +6,11 @@ import { isAuth } from "../middleware/auth.js";
 const route = express.Router();
 
 const validator = [
-  body("username")
-    .notEmpty()
-    .withMessage("username is required")
-    .trim()
-    .isLength({ min: 2 })
-    .withMessage("username should be at least 2 characters"),
-  body("name").notEmpty().withMessage("name is required").trim(),
   body("text")
     .notEmpty()
     .withMessage("text is required")
     .isLength({ min: 3 })
     .withMessage("text should be at least 3 characters"),
-  body("email")
-    .notEmpty()
-    .withMessage("email is required")
-    .trim()
-    .isEmail()
-    .normalizeEmail()
-    .withMessage("invalid email"),
-  body("url"),
   (req, res, next) => {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
