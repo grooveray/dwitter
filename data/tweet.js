217ae01980b1a9a97a596eaed858eb34c2b43760
RiwooPapa <grooveray@gmail.com>
2022-04-12 12:19:23 +0900

    MODIFY/ data/tweets.js all api COMP

--- data/tweet.js
+++ data/tweet.js
@@ -1,26 +1,31 @@
 import * as authRepository from "../data/auth.js";
+import MongoDB from "mongodb";
+import { getTweets, getUsers } from "../database/database.js";
 
-let tweets = [
-  {
-    id: "1",
-    text: "min hong is good",
-    userId: "1",
-  },
-  {
-    id: "2",
-    text: "riddong is great",
-    userId: "2",
-  },
-];
+// let tweets = [
+//   {
+//     id: "1",
+//     text: "min hong is good",
+//     userId: "1",
+//   },
+//   {
+//     id: "2",
+//     text: "riddong is great",
+//     userId: "2",
+//   },
+// ];
 
 export async function getAll() {
-  return Promise.all(
-    tweets.map(async (tweet) => {
-      const { username, name, email, password, url } =
-        await authRepository.findById(tweet.userId);
-      return { ...tweet, username, name, email, password, url };
-    })
-  );
+  const tweets = await getTweets().find().toArray();
+  const auths = await getUsers().find().toArray();
+
+  return tweets.map((tweet) => {
+    const auth = auths.find(
+      (auth) => tweet.userId.toString() === auth._id.toString()
+    );
+    return { ...auth, ...tweet, id: tweet._id.toString() };
+  });
+
   // tweets = await tweets.map(async (tweet) => {
   //   const { username, name, email, password, url } =
   //     await authRepository.findById(tweet.userId);
@@ -44,36 +49,46 @@ export async function getById(id) {
   return found; // return {}
 }
 export async function create(userId, text) {
-  const isMatchedId = async (intId) => {
-    const id = intId.toString();
-    const found = await tweets.find((tweet) => tweet.id === id);
-    return !!found;
-  };
-  const nextId = await (isMatchedId(tweets.length + 1)
-    ? (parseInt(tweets[tweets.length - 1].id) + 1).toString()
-    : (tweets.length + 1).toString());
-  const tweet = {
-    id: nextId,
-    text,
+  const user = {
     userId,
+    text,
     createdAt: Date.now(),
   };
-  tweets.push(tweet);
-  return tweet; // return {}
+  return await getTweets()
+    .insertOne(user)
+    .then((data) => getById(data.insertedId.toString()));
+  // const isMatchedId = async (intId) => {
+  //   const id = intId.toString();
+  //   const found = await tweets.find((tweet) => tweet.id === id);
+  //   return !!found;
+  // };
+  // const nextId = await (isMatchedId(tweets.length + 1)
+  //   ? (parseInt(tweets[tweets.length - 1].id) + 1).toString()
+  //   : (tweets.length + 1).toString());
+  // const tweet = {
+  //   id: nextId,
+  //   text,
+  //   userId,
+  //   createdAt: Date.now(),
+  // };
+  // tweets.push(tweet);
+  // return tweet; // return {}
 }
 export async function update(id, text) {
   const found = getAll().then((tweets) =>
     tweets.find((tweet) => tweet.id === id)
   );
   if (!found) return null;
-  tweets = await tweets.map((tweet) =>
-    tweet.id === id ? { ...tweet, text } : tweet
-  );
-  return tweets; //return []
+
+  return await getTweets()
+    .updateOne({ _id: new MongoDB.ObjectId(id) }, { $set: { text } })
+    .then(() => getAll());
+  // return tweets; //return []
 }
 export async function remove(id) {
-  tweets = getAll().then((tweets) =>
-    tweets.filter((tweet) => !(tweet.id === id))
-  );
+  await getTweets().deleteOne({ _id: new MongoDB.ObjectId(id) });
+  // tweets = getAll().then((tweets) =>
+  //   tweets.filter((tweet) => !(tweet.id === id))
+  // );
   return;
 }
