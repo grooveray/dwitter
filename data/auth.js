c381a912126fa0327f76ba8b21affc9d70370b3b
RiwooPapa <grooveray@gmail.com>
2022-04-15 10:35:11 +0900

    MODIFY/ del comments & READY/ mooongoose

--- data/auth.js
+++ data/auth.js
@@ -1,28 +1,6 @@
 import { getUsers } from "../database/database.js";
 import MongoDB from "mongodb";
 
-// let auths = [
-//   {
-//     username: "ray",
-//     name: "minhong",
-//     email: "suwonchkmh@naver.com",
-//     url: "https://www.naver.com",
-//     password: "$2b$10$SMNd.SfigZtE1igpQeL69ueA/CGA6ovf2tWkAzcM1IMsqfP4i1HNe",
-//     id: "1",
-//   },
-//   {
-//     username: "riddong",
-//     name: "riwoo",
-//     email: "riwoo@gamail.com",
-//     password: "$2b$10$SMNd.SfigZtE1igpQeL69ueA/CGA6ovf2tWkAzcM1IMsqfP4i1HNe",
-//     url: "https://www.google.com",
-//     id: "2",
-//   },
-// ];
-
-export async function getAll() {
-  // return auths;
-}
 export async function findByUsername(username) {
   return getUsers()
     .findOne({ username }) //
